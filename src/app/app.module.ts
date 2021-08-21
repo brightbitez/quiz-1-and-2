921250174e8149935710a4712ce2582f577c8f80
brightbitez <brightbite2009@gmail.com>
2021-08-21 21:26:04 +0700

    finish 1st and 2nd quiz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { Quiz1Component } from './quiz1/quiz1.component';
+import { HttpClientModule } from '@angular/common/http';
+import { Quiz2Component } from './quiz2/quiz2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Quiz1Component,
+    Quiz2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
